9adad9e6dd0c5b36dab10731eaad44850b351b93
aaradhya-shukla <aaradhya.shukla229@gmail.com>
2024-03-03 01:11:48 +0530

    product api

---
+++ models/product.js
@@ -0,0 +1,21 @@
+const getdb = require('../util/database').getDb;
+class Product {
+  constructor(title,price,description,imageUrl){
+    this.title=title;
+    this.description=description;
+    this.imageUrl=imageUrl;
+    this.price=price;
+  }
+  save(){
+    const db = getdb();
+    return db.collection('products').insertOne(this).
+    then(result=>{
+      console.log(result)
+    }).
+    catch(err=>{
+      console.log(err)
+    });
+  }
+}
+
+module.exports = Product;
